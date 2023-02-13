c968d18795730cb71a71f0aa7572322567cdd8a6
fyezhlo <fyezhlo@gmail.com>
2023-02-13 19:40:55 +0300

    dependencies added, logger addet, project built

--- src/test/java/ru/fyodor/AppTest.java
+++ src/test/java/ru/fyodor/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
  * Unit test for simple App.
  */
@@ -12,9 +14,17 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void logTest() throws Exception {
+        Logger logger = LogManager.getLogger(App.class.getName());
+        logger.error("testing ERROR message log");
     }
 }
